e650c895ea87227b938d81a200d1428f7e10e155
Alejo Valls Cura <alejo.valls.c@gmail.com>
2022-08-12 21:01:21 -0300

    Revert "Ultima entrega"
    This reverts commit fd1d0fdded53f514500a0b3067a4ff8a8d0a7e5c.

--- proyecto/js/tienda.js
+++ proyecto/js/tienda.js
@@ -15,14 +15,6 @@ const zapatillas = [{
         image: "<img src='../imagenes/zapatillas/wjffl5e1.bmp' />"
     },
     {
-        id: 10,
-        marca: "Puma",
-        categoria: "",
-        nombre: "Pmas 102",
-        precio: "19.500",
-        image: "<img src='../imagenes/zapatillas/puma.png' />"
-    },
-    {
         id: 3,
         marca: "Nike",
         categoria: "",
@@ -32,11 +24,11 @@ const zapatillas = [{
     },
     {
         id: 4,
-        marca: "Shoes",
+        marca: "Asics",
         categoria: "",
         nombre: "EQ78",
         precio: "19.000",
-        image: "<img src='../imagenes/zapatillas/shoes.png' />"
+        image: "<img src='../imagenes/zapatillas/zapatillas-deportivas-moda-11910482-removebg-preview.png' />"
     },
     {
         id: 5,
@@ -47,20 +39,12 @@ const zapatillas = [{
         image: "<img src='../imagenes/zapatillas/wjffl5e1.bmp' />"
     },
     {
-        id: 11,
-        marca: "Puma",
-        categoria: "",
-        nombre: "P Max",
-        precio: "13.900",
-        image: "<img src='../imagenes/zapatillas/puma.png' />"
-    },
-    {
         id: 6,
-        marca: "Shoes",
+        marca: "Asics",
         categoria: "",
         nombre: "Run-T",
         precio: "18.000",
-        image: "<img src='../imagenes/zapatillas/shoes.png' />"
+        image: "<img src='../imagenes/zapatillas/zapatillas-deportivas-moda-11910482-removebg-preview.png' />"
     },
     {
         id: 7,
@@ -69,14 +53,6 @@ const zapatillas = [{
         nombre: "Air Force 1",
         precio: "14.900",
         image: "<img src='../imagenes/zapatillas/otroLogo-removebg-preview.png' />"
-    },
-    {
-        id: 13,
-        marca: "NewBalance",
-        categoria: "",
-        nombre: "NewStyle",
-        precio: "22.300",
-        image: "<img src='../imagenes/zapatillas/newbal-removebg-preview.png' />"
     },
     {
         id: 8,
@@ -90,81 +66,71 @@ const zapatillas = [{
         id: 9,
         marca: "Nike",
         categoria: "",
-        nombre: "AirMax97",
+        nombre: "Revolution",
         precio: "22.300",
         image: "<img src='../imagenes/zapatillas/otroLogo-removebg-preview.png' />"
-    },
-    {
-        id: 14,
-        marca: "NewBalance",
-        categoria: "",
-        nombre: "Insan",
-        precio: "22.300",
-        image: "<img src='../imagenes/zapatillas/newbal-removebg-preview.png' />"
-    },
-    {
-        id: 12,
-        marca: "Puma",
-        categoria: "",
-        nombre: "Pumas 311",
-        precio: "27.500",
-        image: "<img src='../imagenes/zapatillas/puma.png' />"
-    },
-    {
-        id: 4,
-        marca: "Shoes",
-        categoria: "",
-        nombre: "32 Max",
-        precio: "9.000",
-        image: "<img src='../imagenes/zapatillas/shoes.png' />"
     }
 ];
 
 //  FILTRTADO DE ZAPATILLAS
 
-function pintarProducto(productos) {
-    let zapas = document.querySelector("#zapatilla");
-    zapas.innerHTML = ""
-    productos.forEach(i => {
-        zapas.innerHTML += `
-        <div class="d_articulos"> 
-            <p class="p_articulos"> 
-                <h5>${i.marca} ${i.nombre}</h5> 
-                <h4>$ ${i.precio}</h4>  
-                <p>${i.image}</p> 
-            </p>
-                <button id="${i.id}" class="buy-btn botonCompra">Agregar al Carrito</button>
-        </div>`
-    });
-}
-pintarProducto(zapatillas);
-let boton_nike = document.getElementById("boton_nike");
-boton_todas.addEventListener("click", function () {
-    pintarProducto(zapatillas);
-})
-boton_nike.addEventListener("click", function () {
-    let nikes = zapatillas.filter(e => e.marca === "Nike");
-    pintarProducto(nikes);
-})
-boton_adidas.addEventListener("click", function () {
-    let adi = zapatillas.filter(e => e.marca === "Adidas");
-    pintarProducto(adi);
-})
-boton_shoes.addEventListener("click", function () {
-    let sho = zapatillas.filter(e => e.marca === "Shoes");
-    pintarProducto(sho);
-})
-boton_puma.addEventListener("click", function () {
-    let pum = zapatillas.filter(e => e.marca === "Puma");
-    pintarProducto(pum);
-})
-boton_newBalance.addEventListener("click", function () {
-    let newB = zapatillas.filter(e => e.marca === "NewBalance");
-    pintarProducto(newB);
-})
+let zapas = document.querySelector("#zapatilla");
+
+zapatillas.forEach(i => {
+    zapas.innerHTML += `<div class="d_articulos"> <p class="p_articulos"> <h5>${i.marca} ${i.nombre}</h5> <h4>$ ${i.precio}</h4>  <p>${i.image}</p> </p>
+    <button id="agregarModal" class="buy-btn botonCompra">Agregar al Carrito</button> </div>`
+});
+
+
+// let boton_todas = document.getElementById("boton_todas");
+// boton_todas.addEventListener("click", function () {
+//     zapas.innerHTML = ""
+//     zapatillas.forEach(i => {
+//         zapas.innerHTML += `<div class="d_articulos"> <p class="p_articulos"> <h5>${i.marca} ${i.nombre}</h5> <h4>$${i.precio}</h4> <p>${i.image}</p> </p>
+//         <button id="agregarModal" class="buy-btn botonCompra">Agregar al Carrito</button> </div>`
+//     });
+// });
+
+// let boton_nike = document.getElementById("boton_nike");
+// boton_nike.addEventListener("click", function () {
+//     zapas.innerHTML = ""
+//     const filtro = zapatillas.filter(filtradoZapas => filtradoZapas.marca === "Nike");
+//     console.log(filtro);
+//     filtro.forEach(i => {
+//         zapas.innerHTML += `<div class="d_articulos"> <p class="p_articulos"> <h5>${i.marca} ${i.nombre}</h5> <h4>$${i.precio}</h4> <p>${i.image}</p>  </p> 
+//         <button class="buy-btn botonCompra">Agregar al Carrito</button></div>`
+//     });
+// });
+
+// let boton_adidas = document.getElementById("boton_adidas");
+// boton_adidas.addEventListener("click", function () {
+//     zapas.innerHTML = ""
+//     const filtro = zapatillas.filter(filtradoZapas => filtradoZapas.marca === "Adidas");
+//     console.log(filtro);
+//     filtro.forEach(i => {
+//         zapas.innerHTML += `<div class="d_articulos"> <p class="p_articulos"> <h5>${i.marca} ${i.nombre}</h5> <h4>$${i.precio}</h4> <p>${i.image}</p>  </p> 
+//         <button class="buy-btn botonCompra">Agregar al Carrito</button></div>`
+//     });
+// });
+
+
+// let boton_ascics = document.getElementById("boton_ascics");
+// boton_ascics.addEventListener("click", function () {
+//     zapas.innerHTML = ""
+//     const filtro = zapatillas.filter(filtradoZapas => filtradoZapas.marca === "Asics");
+//     console.log(filtro);
+//     filtro.forEach(i => {
+//         zapas.innerHTML += `<div class="d_articulos"> <p class="p_articulos"> <h5>${i.marca} ${i.nombre}</h5> <h4> $${i.precio}</h4> <p>${i.image}</p>  </p>
+//         <button class="buy-btn botonCompra">Agregar al Carrito</button></div>`
+//     });
+// });
+
+
 
 
 //  BOTON COMPRAR - CARRITO 
+
+let carrito = [];
 
 function traerLocalStorage() {
     carrito = JSON.parse(localStorage.getItem("carrito")) || [];
@@ -179,21 +145,17 @@ for (let boton of btn_compra) {
 
 
 function agregar_al_carrito(e) {
-    let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
     let padre = e.target.parentNode;
 
     let nombre_producto = padre.querySelector("h5").textContent;
     let precio_producto = padre.querySelector("h4").textContent;
     let img_producto = padre.querySelector("img").src;
-    let id_producto = padre.querySelector(".buy-btn").id;
-
 
     let producto = {
         nombre: nombre_producto,
         precio: precio_producto,
         cantidad: 1,
-        img: img_producto,
-        id: id_producto
+        img: img_producto
     };
 
     carrito.push(producto);
@@ -201,25 +163,30 @@ function agregar_al_carrito(e) {
     let arreglo_JSON = JSON.stringify(carrito);
     localStorage.setItem("carrito", arreglo_JSON);
 
-    const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 1200,
-        timerProgressBar: true
+
+    Swal.fire({
+        title: 'Bien',
+        text: 'Se agrego producto al carrito!',
+        icon: 'success'
     })
 
-    Toast.fire({
-        icon: 'success',
-        title: 'Producto agregado al carrito'
-    })
+    let botones_borrar = document.querySelectorAll('.btn-borrar');
+    for (const boton of botones_borrar) {
+        boton.addEventListener('click', borrar_producto);
+    }
 }
 
+function borrar_producto(e) {
+    let producto = e.target.parentNode.parentNode 
+    producto.remove();
+    localStorage.clear();
+    let arreglo_JSON = JSON.stringify(carrito);
+    localStorage.setItem("carrito", arreglo_JSON);
+}
 
 document.getElementById("carroBoton").addEventListener("click", mostrarCarrito);
 
 function mostrarCarrito() {
-
     let main = document.getElementById("main");
     main.innerHTML = ''
     let carritoContainer = document.createElement('div');
@@ -249,73 +216,64 @@ function mostrarCarrito() {
 
     main.appendChild(carritoContainer);
 
-    let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
     carrito.forEach(e => {
         let fila = document.createElement("tr");
         let tabla = document.getElementById("tbody");
 
-        fila.innerHTML = `<td><a id="${e.id}" class="fa-solid fa-trash-can borrar_elemento"></a></td>
+        fila.innerHTML = `<td><a class="fa-solid fa-trash-can borrar_elemento"></a></td>
         <td><img src="${e.img}" height=60px></td>
         <td>${e.nombre} </td>
         <td>${e.cantidad} </td>
         <td>${e.precio} </td>`;
 
+
+        let botones_borrar = document.querySelectorAll(".borrar_elemento");
+        for (let btn of botones_borrar) {
+            btn.addEventListener("click", borrar_producto)
+        }
         tabla.append(fila);
     });
 
-    let botones_borrar = document.querySelectorAll(".borrar_elemento");
-    for (let btn of botones_borrar) {
-        btn.addEventListener("click", borrar_producto)
-    }
 
     const footer = document.getElementById('footer');
 
     footer.innerHTML = ''
+
     if (carrito.length === 0) {
-        footer.innerHTML = `<th scope="row" colspan="5" class="carritoVacio text-center ">   -   Carrito Vacio   -   <a class="botonVolverTienda" href="./tienda.html">  Comprar ahora  </a> -</th>`
+            footer.innerHTML = `<th scope="row" colspan="5" class="carritoVacio text-center ">   -   Carrito Vacio   -   <a class="botonVolverTienda" href="./tienda.html">  Comprar ahora  </a> -</th>`
         return
     }
 
-
-    if (carrito.length > -1) {
-
-        footer.innerHTML = `
-        <td><button class="btn btn-danger btn-sm" id="vaciar_carro">Vaciar todo</button></td>
-        <td><button class="btn btn-success btn-sm" id="comprar">Comprar</button></td>
-        <td>Total productos</td>
-        <td>${carrito.length}</td>
-        <td class="font-weight-bold"> $</td>`
-
-        let boton_vaciar = document.getElementById("vaciar_carro");
-        boton_vaciar.addEventListener("click", vaciar_carrito);
-
-        let boton_comprar = document.getElementById("comprar");
-        boton_comprar.addEventListener("click", hacer_checkout);
-
+    if (carrito.length > 0) {
+            footer.innerHTML = `
+            <td><button class="btn btn-danger btn-sm" id="vaciar_carro">Vaciar todo</button></td>
+            <td><button class="btn btn-success btn-sm" id="">Comprar</button></td>
+            <td>Total productos</td>
+            <td>${carrito.length}</td>
+            <td class="font-weight-bold"> $</td>`
+            let boton_vaciar = document.getElementById("vaciar_carro");
+            boton_vaciar.addEventListener("click", vaciar_carrito);
+        return  
     }
+    
+   
 }
 
 
-function borrar_producto(e) {
 
-    let producto = e.target.parentNode.parentNode
-    producto.remove();
 
-    let productosLS = JSON.parse(localStorage.getItem('carrito'));
+// function borrar_producto(e) {
 
-    if (productosLS) {
-        let newProductos = productosLS.filter(producto => producto.id !== e.target.id);
-        localStorage.setItem('carrito', JSON.stringify(newProductos));
-    }
-
-    Swal.fire({
-        icon: 'error',
-        title: 'Se elimino un producto',
-        text: 'Que lastima!'
-    })
-    mostrarCarrito()
-}
+//     // localStorage.removeItem("carrito", e.target);
+//     //  COMO HAGO PARA BORRAR 1 SOLO
+//     mostrarCarrito(traerLocalStorage());
+//     Swal.fire({
+//         icon: 'error',
+//         title: 'Se elimino un producto',
+//         text: 'Que lastima!'
+//     })
+// }
 
 function vaciar_carrito(e) {
     localStorage.clear();
@@ -327,23 +285,45 @@ function vaciar_carrito(e) {
     })
 }
 
-function hacer_checkout() {
-        Swal.fire({
-        title: 'Multiple inputs',
-        html:
-          '<input id="swal-input1" class="swal2-input">' +
-          '<input id="swal-input2" class="swal2-input">',
-        focusConfirm: false,
-        preConfirm: () => {
-          return [
-            document.getElementById('swal-input1').value,
-            document.getElementById('swal-input2').value,
-            localStorage.clear()
-          ]
-        }
-      })
-      
-      if (formValues) {
-        Swal.fire(JSON.stringify(formValues))
-      }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let zapas = document.querySelector("#zapatilla");
+// const btnsFilter = document.getElementsByClassName("btn-filter");
+// function mostrarProductos(productos) {
+//     zapas.innerHTML = ""
+//     productos.forEach(i => {
+//         zapas.innerHTML += `<div class="d_articulos"> <p class="p_articulos"> <p>${i.marca}</p> <p>${i.nombre}</p> <p>${i.image}</p>  </p> </div>`
+//     });
+// }
+// mostrarProductos(zapatillas)
+// for(btn of btnsFilter){
+//     btn.addEventListener("click", (e)=>{
+//         console.log(e.target.value)
+//         let filtro=e.target.value
+//         if(filtro==="todas"){
+//             mostrarProductos(zapatillas)
+//             return //corto de flujo
+//         }
+//         const filtradas = zapatillas.filter(filtradoZapas => filtradoZapas.marca === filtro)
+//         mostrarProductos(filtradas)
+//     })
+// }
+// const doc = document;
+// let zapas = document.querySelector("#zapatilla");
+// zapatillas.forEach(i => {
+//     zapas.innerHTML += `<div class="d_articulos"> <p class="p_articulos"> <p>${i.marca}</p> <p>${i.nombre}</p> <p>${i.image}</p>  </p> </div>`
+// });
